a3a0f2bfdb1208cef683a70f66059a3b3b9666b4
Md Soriful hasan <sorifullhasan300@gmail.com>
2026-01-11 20:00:17 +0600

    impliment book controller

---
+++ src/controllers/book.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import Book from "../models/Book";
+
+export const createBook = async (req: Request, res: Response) => {
+  const book = await Book.create({
+    ...req.body,
+    coverImage: req.file?.path,
+  });
+  res.status(201).json(book);
+};
+
+export const getBooks = async (_: Request, res: Response) => {
+  const books = await Book.find().populate("genre");
+  res.json(books);
+};
+
+export const updateBook = async (req: Request, res: Response) => {
+  const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.json(book);
+};
+
+export const deleteBook = async (req: Request, res: Response) => {
+  await Book.findByIdAndDelete(req.params.id);
+  res.json({ message: "Book deleted" });
+};
