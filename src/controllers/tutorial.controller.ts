a8a2af140dc7b357ec9bb369294f588309aa6d67
Md Soriful hasan <sorifullhasan300@gmail.com>
2026-01-12 20:26:06 +0600

    validation user and book exist when add tutorial

--- src/controllers/tutorial.controller.ts
+++ src/controllers/tutorial.controller.ts
@@ -1,7 +1,23 @@
 import { Request, Response } from "express";
 import { Tutorial } from "../models/Tutorial";
+import { Types } from "mongoose";
+import Book from "../models/Book";
+import User from "../models/User";
 
 export const createTutorial = async (req: any, res: Response) => {
+  if (req.body.book && !Types.ObjectId.isValid(req.body.book)) {
+    return res.status(400).json({
+      message: "Invalid book id format",
+    });
+  }
+  const book = await Book.findById(req.body.book);
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    return res.status(401).json({ message: "Invalid user" });
+  }
   const tutorial = await Tutorial.create({
     title: req.body.title,
     youtubeUrl: req.body.youtubeUrl,
